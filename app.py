b4577c379fdd227ad2ce05e28b04d5cc57193364
Aeron Roemer <aeronroemer@users.noreply.github.com>
2021-01-09 11:49:14 -0700

    Stocks list added to app.py .

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-#imports 
+# imports 
+import pandas as pd
 import random
 
-num_of_stocks = int(input("How many different shares of stocks do you want? "))   # number of desired stocks
+num_of_stocks = 30   # number of desired stocks
+
+# read CSV with pandas, extract one column as stock list
+stock_csv = pd.read_csv ('./NASDAQ_full.csv')
+stock_list= list(stock_csv['Symbol'])
+
+print (stock_list)
