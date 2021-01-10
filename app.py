4c7e481aec355f3d644842b1caa8dc4de326421a
Aeron Roemer <aeronroemer@users.noreply.github.com>
2021-01-10 14:49:07 -0700

    adds random selection functionality and creates list of 30 stocks

--- app.py
+++ app.py
@@ -8,4 +8,6 @@ num_of_stocks = 30   # number of desired stocks
 stock_csv = pd.read_csv ('./NASDAQ_full.csv')
 stock_list= list(stock_csv['Symbol'])
 
-print (stock_list)
+# select 30 random stocks and store in list
+chosen_stock_list = random.sample(stock_list, 30)
+print (chosen_stock_list)
